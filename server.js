d28b1fca250898fe95ca6eaed52b8e7e43f81ece
Misha Balin <m.balin@icloud.com>
2019-04-13 17:16:50 +0300

    Build

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const server = module.exports.server = exports.server = express();
 server.use(express.static(__dirname + '/'));
 server.listen(8081);
 
-console.log('Server on port 8081 runed');
+console.log('Ready on http://localhost:8081/');
