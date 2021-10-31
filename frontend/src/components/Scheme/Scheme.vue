0ddedd458f0d1ed198192c8eeec399af6b1cd693
Andrei <1328bas@gmail.com>
2021-10-31 20:57:31 +0300

    onhover styles reorginized

--- frontend/src/components/Scheme/Scheme.vue
+++ frontend/src/components/Scheme/Scheme.vue
@@ -5,11 +5,11 @@ export default {
     name: 'scheme',
     methods: {
         hoverOn (event, num) {
-            event.target.parentElement.classList.add(`on-hover-scale__${num}`)
+            event.target.parentElement.classList.add('on-hover-scale', `on-hover-scale__${num}`)
             event.target.parentElement.lastChild.style.display = 'none'
         },
         hoverOff (event, num) {
-            event.target.parentElement.classList.remove(`on-hover-scale__${num}`)
+            event.target.parentElement.classList.remove('on-hover-scale', `on-hover-scale__${num}`)
             event.target.parentElement.lastChild.style.display = 'block'
         },
         mounted () {}
