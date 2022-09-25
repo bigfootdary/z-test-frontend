170da11b71195287374c395bfaf9c1334b3de314
choboss <93683538+andreyVoronov@users.noreply.github.com>
2022-09-25 23:06:24 +0300

    fix

--- frontend/src/store/modules/news.js
+++ frontend/src/store/modules/news.js
@@ -8,21 +8,22 @@ export default {
        state.posts = posts.items;
        state.nav = posts.nav;
     },
-     concatePosts(state,Newposts,nav) {
+     concatePosts(state,Newposts) {
       let arr1 = Newposts.items;
       let arr2 = state.posts;
       arr2 = [...arr2, ...arr1];
       state.posts = arr2
       state.nav.current = state.nav.current + 1
-     }
+     },
   },
   actions: { 
   async fetchPosts({ commit }, currentPage = 1) {
-    const res = await fetch (
-      "http://localhost:8081/api/news/" + currentPage 
-  );
-    const posts = await res.json();
-    commit('updatePosts', posts)
+    return fetch("http://localhost:8081/api/news/" + currentPage)
+    .then((response) => response.json())
+    .then((posts) => {
+      commit("updatePosts", posts);
+    })
+    .catch((err) => console.error(err));      
    },
    async getNextPage({ commit, getters }) {
     const res = await fetch (
--- frontend/src/views/List/List.vue
+++ frontend/src/views/List/List.vue
@@ -1,14 +1,22 @@
      <template>
         <div class="news-block">
             <div class="news-list">
-               <div class="news-card" v-for="post in allPosts" :key="post.id">
-                  <span>{{ post.date | momentFilter }}</span>
+               <div class="news-card"
+                 :class="{spotlight: post.spotlight}"
+                 v-for="post in allPosts" :key="post.id">
+                  <div class="news-date">
+                    <span class="date">{{ post.date | momentFilter }}</span>
+                  </div>
                   <span v-if="post.spotlight">В центре внимания</span>
-                  <h2>{{ post.title }}</h2>
+                  <h2 class="title">{{ post.title }}</h2>
+                  <span v-if="post.theme" class="theme-title">{{ post.theme[0] }}</span>
                </div>
             </div>
-            <button v-if="$store.getters.currentPage !== $store.getters.totalPages" 
-             v-on:click="getNextPage">Next{{ currentPage }}</button>
+            <div class="load">
+                <button class="load-button"
+                 v-if="$store.getters.currentPage !== $store.getters.totalPages" 
+                 v-on:click="getNextPage">Загрузить ещё</button>
+            </div>
         </div>
     </template>
 
