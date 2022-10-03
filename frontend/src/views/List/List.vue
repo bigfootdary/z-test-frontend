b96d00f3f31d53bcf88ce74f1cfd4438b6482427
ekatrif <katiapervak@gmail.com>
2022-10-03 12:23:19 +0300

    feat: add mobile layout for news list

--- frontend/src/views/List/List.vue
+++ frontend/src/views/List/List.vue
@@ -4,7 +4,8 @@
             <div class="news__item" v-for="post in news.items" :key="post.id">
                 <a :href="post.url">
                     <div class="item">
-                <img v-show="post.image" :src="post.image" alt="" />
+                <!-- <img v-show="post.image" :src="post.image" alt="" /> -->
+                <div class="item__img" :style="{backgroundImage: 'url(' + post.image + ')'}"></div>
                 <div class="item__date date">
                     <div class="date__wrapper">
                         <span>{{post.date | momentFilter}}</span>
@@ -48,11 +49,9 @@ export default {
 .news__button {
     margin-top: 20px;
 }
-
 .news {
     display: grid;
     gap: $grid-margin;
-    justify-content: space-between;
     grid-template-columns: repeat(3, 1fr);
 }
 .item {
@@ -64,26 +63,24 @@ export default {
     min-height: 254px;
     height: 100%;
     background-color: $alabaster;
-    overflow: hidden;
-    & img {
-    display:block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    z-index: -1;
-    // transition: 1s;
-}
-
+    overflow: hidden;   
     &:hover {
         cursor:pointer;
     }
 //     &:hover & img {
 //     transform: scale(1.1);
 // }
-    
+}
+.item__img {
+    display:block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    // transition: 1s;
 }
 .news__item {
     & > a {text-decoration: none;}
@@ -129,7 +126,6 @@ export default {
         padding: 2px 8px;
     }
 }
-
 .item__title {
     margin-top: 30px;
     margin-bottom: 46px;
@@ -154,4 +150,76 @@ export default {
 .tag__wrapper span {
     padding: 0px 5px;
 }
+
+// Мобильная версия //
+@media(max-width:$breakpoint-mobile) {
+    .item__img {
+    display:none;
+}
+    .news {
+    display: grid;
+    gap: $grid-margin-mobile;
+    grid-template-rows: repeat(auto-fit, 1fr);
+    grid-template-columns: 1fr;
+}
+.item {
+    padding: 0 0 23px 0;
+    min-height: auto;
+    background-color: transparent;
+}
+    &:hover {
+        cursor:pointer;
+    }
+.date__wrapper {
+    span {
+        padding: 0px 4px;
+    }
+}
+.news__item,.news__item:first-child {
+        & a .item{
+        position:relative;
+        padding: 0 0 21px 0;
+        &::after {
+            position:absolute;
+            content:'';
+            bottom:0;
+            left:0;
+            width:100%;
+            height:1px;
+            background-color:$quill-gray;
+        }
+        & .item__title {
+            order: 2;
+            margin: 2px 0 14px 0;
+            font-weight: 400;
+            font-size: $kilo-third;
+            line-height: $kilo-third * 1.25;
+            color: $black;
+            text-transform: none;
+        }        
+        & .item__tag {
+            order: 3;
+            margin-top: auto;
+            margin-bottom: 0;
+        }
+        & .tag__wrapper {
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            color: $cool-gray;
+            font-size: $teta-size;
+            font-weight: 700;
+            text-transform: none;
+            border: 1px solid $cool-gray-light;
+            border-radius: 2px;
+        }
+    }}
+    .news__item:first-child {
+        grid-column: 1 / 2;
+    }
+    .news__button {
+    margin-top: 25px;
+}
+.button--more-news {width:100%}
+    }
 </style>
